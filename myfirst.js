8c67d9dd284eff8f512a92dbed12bfcb6b2f200f
Tyler White <7670501@isd535.org>
2022-04-26 10:36:24 -0500

    using html

--- myfirst.js
+++ myfirst.js
@@ -1,10 +1,18 @@
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
     res.writeHead(200, {'Content-Type': 'text/html'});
-    var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
-    res.end(txt);
-  }).listen(8080);
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
